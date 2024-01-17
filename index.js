f92e23f59f0be467b0a6c7395a5c180929c78a4f
sabrina <sabrinaahmednitu@gmail.com>
2024-01-17 23:29:17 +0600

    wright code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 app.use(cors());
 app.use(express.json());
@@ -32,19 +32,14 @@ async function run() {
       const products = await cursor.toArray();
       res.send(products);
     });
-    
+
     //fetch all data here
-    app.get('/electronicProduct', async (req, res) => {
-      const query = {};
-      const cursor = electronicProductCollection.find(query);
-      const products = await cursor.toArray();
-      res.send(products);
+    app.get('/electronicProduct/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }; // Convert the id to ObjectId
+      const product = await electronicProductCollection.findOne(query);
+      res.send(product);
     });
-  
-  
-  
-  
-  
   
   
   }
